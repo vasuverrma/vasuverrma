f5366a6f2f9b61207e405393d1e337632a9defaa
Vasuverrma <vasuverma487@gmail.com>
2022-08-24 02:28:10 +0530

    Sales Information Project

---
+++ Sales_and_Information_System/README.md.txt
@@ -0,0 +1,29 @@
+## Steps to get started
+
+1. FIRST Download XAMPP
+
+2. "TEXT EDITOR" NOTEPAD++ OR SUBLIME TEXT 3 / ETC.
+
+3. "Sales_and_Inventory_System_by_VasuVerma"
+
+4. Download the zip file/ download winrar
+
+5. Extract the file and copy "Sales_and_Information_System" folder
+
+6. Paste inside root directory/ where you install xammp local disk C: drive D: drive E: paste: (for xampp/htdocs, 
+
+7. Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+8. Create a database with name prince
+
+6. Import prince.sql file(given inside the zip package in SQL file folder)
+
+7. Run the script http://localhost/Sales_and_Information_System
+
+**LOGIN DETAILS** 
+
+admin
+user: admin
+pass: admin
+
+
---
+++ Sales_and_Information_System/js/city.js
@@ -0,0 +1,90 @@
+/**
+* Philippine Provinces & Cities/Municipalities on HTML Dropdown
+*
+* @ version 1.0.0
+* @ author Marvic R. Macalintal
+*/
+ var cities = {
+	'Punjab' : [
+		'Fazilka', 'Abohar', 'Patiala','Amritsar','Barnala','Gurdaspur'
+		],
+	'Haryana' : [
+		'Ambala','Faridabad','Fatehabad','Sirsa','Hisar','Gurgaon'
+		],
+	'Rajasthan' : [
+		'Jhunjhnu','Bikaner','Ganganagar','Hanumangarh','Jaipur','Jaisalmer'
+		],
+	'Gujrat' : [
+		'Ahmedabad','Amreli','Anand','Aravali','Palanpur','Rajkot','Surat',
+		],
+	'Maharashtra' : [
+		'Ahmednagar','Akola','Amravati','Aurangabad','Bhandara','Chandrapur','Kohlapur','Nagpur'
+		],
+	'Jammu And Kashmir' : [
+		'Balakot','Poonch','Srinagar','Pulwama'
+		],		
+ }
+
+ var City = function() {
+
+	this.p = [],this.c = [],this.a = [],this.e = {};
+	window.onerror = function() { return true; }
+	
+	this.getProvinces = function() {
+		for(let province in cities) {
+			this.p.push(province);
+		}
+		return this.p;
+	}
+	this.getCities = function(province) {
+		if(province.length==0) {
+			console.error('Please input province name');
+			return;
+		}
+		for(let i=0;i<=cities[province].length-1;i++) {
+			this.c.push(cities[province][i]);
+		}
+		return this.c;
+	}
+	this.getAllCities = function() {
+		for(let i in cities) {
+			for(let j=0;j<=cities[i].length-1;j++) {
+				this.a.push(cities[i][j]);
+			}
+		}
+		this.a.sort();
+		return this.a;
+	}
+	this.showProvinces = function(element) {
+		var str = '<option selected disabled>Select State</option>';
+		for(let i in this.getProvinces()) {
+			str+='<option>'+this.p[i]+'</option>';
+		}
+		this.p = [];		
+		document.querySelector(element).innerHTML = '';
+		document.querySelector(element).innerHTML = str;
+		this.e = element;
+		return this;
+	}
+	this.showCities = function(province,element) {
+		var str = '<option selected disabled>Select City</option>';
+		var elem = '';
+		if((province.indexOf(".")!==-1 || province.indexOf("#")!==-1)) {
+			elem = province;
+		}
+		else {
+			for(let i in this.getCities(province)) {
+				str+='<option>'+this.c[i]+'</option>';
+			}
+			elem = element;
+		}
+		this.c = [];
+		document.querySelector(elem).innerHTML = '';
+		document.querySelector(elem).innerHTML = str;
+		document.querySelector(this.e).onchange = function() {		
+			var Obj = new City();
+			Obj.showCities(this.value,elem);
+		}
+		return this;
+	}
+}
